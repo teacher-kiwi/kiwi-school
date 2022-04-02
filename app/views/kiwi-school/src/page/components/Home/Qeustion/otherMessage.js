183a4c27124cb6321b8d264f3e66b1d97e375570
teacher-kiwi <kwi07@hanmail.net>
2022-04-02 22:31:13 +0900

    질문 기능 오류 수정

--- app/views/kiwi-school/src/page/components/Home/Qeustion/otherMessage.js
+++ app/views/kiwi-school/src/page/components/Home/Qeustion/otherMessage.js
@@ -3,7 +3,7 @@ import { Row } from "react-bootstrap";
 export default function OtherMsg({ writer, time, text }) {
   return (
     <>
-      <span className="ms-2">김우일</span>
+      <span className="ms-2">{writer}</span>
       <Row className="d-flex m-2 justify-content-start align-items-center">
         <p
           className="w-auto m-0 p-3 shadow-sm"
